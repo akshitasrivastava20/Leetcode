5112807db63d7f6e1db3b6b1e49a48ea3a219375
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2025-11-17 23:19:44 +0530

    Time: 0 ms (100%), Space: 45.2 MB (20.28%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -8,25 +8,17 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+ class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        
         ListNode temp=head;
-        if(temp==null) return null;
-
-        while(temp.next!=null){
-            if(temp.val==temp.next.val) {
-                temp.next=temp.next.next;
-                
-            }
-
-            else{
-                temp=temp.next;
-            }
-
-
-
+        if(temp==null) return temp;
+        while(temp!=null&&temp.next!=null){
+            if(temp.val==temp.next.val){
+            temp.next=temp.next.next;
+        }
+        else{temp=temp.next;}
         }
         return head;
+        
     }
 }
