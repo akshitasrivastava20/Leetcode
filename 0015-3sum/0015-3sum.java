593208967dda7888670057372e1650f249c36e53
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-01-23 14:35:08 +0530

    Time: 62 ms (19.39%), Space: 59.5 MB (25.82%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,39 +1,30 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums); // Sorting is essential for two-pointer logic
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Avoid duplicates for i
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // Skip duplicates for left and right
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                } 
-                else if (sum < 0) {
-                    left++;
-                } 
-                else {
-                    right--;
+        
+       
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+       
+        for(int k=0;k<nums.length-2;k++){
+            int s=k+1;int e=nums.length-1;
+            while(s<e){
+                if(nums[s]+nums[e]==(-1)*nums[k]) {
+                    ans.add(new ArrayList<>(Arrays.asList(nums[s],nums[e],nums[k])));
+                    while (s < e && nums[s] == nums[s + 1]) s++;
+                    while (s < e && nums[e] == nums[e - 1]) e--;
+                    s++;
+                    e--;
                 }
-            }
-        }
+                else if(nums[s]+nums[e]>(-1)*nums[k]) e--;
+                else s++;
 
-        return res;
+            }
+            
+
+        }
+        List<List<Integer>> unique = new ArrayList<>(new HashSet<>(ans));
+
+        return unique;
+
     }
 }
