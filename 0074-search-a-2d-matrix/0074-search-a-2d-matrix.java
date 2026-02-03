287b3172ea2702fe2a5759f86d818c8e4f80e9e0
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-02-03 21:00:21 +0530

    Time: 0 ms (100%), Space: 44.3 MB (6.38%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix[0].length-1;
+        for(int i=0;i<matrix.length;i++){
+            int l=0;
+            int h=n;
+            if(matrix[i][h]<target && i<matrix.length-1) i++;
+            while(l<=h){
+                
+                int mid=(l+h)/2;
+                if(matrix[i][mid]==target) return true;
+                else if(matrix[i][mid]>target) h=mid-1;
+                else l=mid+1;
+            }
+        }
+        return false;
+    }
+}
