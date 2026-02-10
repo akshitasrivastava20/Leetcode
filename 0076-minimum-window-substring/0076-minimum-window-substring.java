832b0a191fb7ed417a62edb97693441e997ce349
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-02-10 11:36:35 +0530

    Time: 15 ms (62.5%), Space: 47 MB (59.04%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,32 +1,39 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
-        HashMap<Character,Integer> mp=new HashMap<>();
-        for(char ch:t.toCharArray()){
-            mp.put(ch,mp.getOrDefault(ch,0)+1);
+        HashMap<Character, Integer> mp = new HashMap<>();
+        int l = 0;
+        int r = 0;
+        int minstart=0;
+        int minlen = Integer.MAX_VALUE;
+        int count = 0;
+        // make hashmap of t
+        for (char c : t.toCharArray()) {
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
         }
-        int l=0;int r=0;int count=0;int min_start=0;int min_len=Integer.MAX_VALUE;
-        while(r<s.length()){
-            mp.put(s.charAt(r),mp.getOrDefault(s.charAt(r),0)-1);
-            if(mp.get(s.charAt(r))>=0) count++;
-            
-            while(count==t.length()){
-                if(r-l+1<min_len){
-                    min_len=r-l+1;
-                    min_start=l;
+
+        //traverse over entire s 
+        while (r < s.length()) {
+            char ch = s.charAt(r);
+            mp.put(ch, mp.getOrDefault(ch, 0) - 1);
+            if (mp.get(ch) >= 0)
+                count++;
+
+            while (count == t.length()) {
+                if (r - l + 1 < minlen) {
+                    minlen = r - l + 1;
+                    minstart=l;
+
                 }
-                
-                    mp.put(s.charAt(l),mp.get(s.charAt(l))+1);
-                    if(mp.get(s.charAt(l))>0) {
-                       count--;
-                    }
-                    l++;
-                
+                mp.put(s.charAt(l),mp.get(s.charAt(l))+1);
+                if(mp.get(s.charAt(l))>0) count--;
+                l++;
 
             }
             r++;
+            
         }
-        return min_len == Integer.MAX_VALUE ? "" 
-               :  s.substring(min_start,min_start+min_len);
+        return minlen == Integer.MAX_VALUE ? "" 
+               :  s.substring(minstart,minstart+minlen);
+
     }
 }
