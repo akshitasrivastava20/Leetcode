5df5641808c991d49fe39f9586fd737eff24235a
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-02-24 09:21:21 +0530

    Time: 0 ms (100%), Space: 46.6 MB (87.3%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,13 +13,15 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         if(head==null||head.next==null) return false;
         ListNode s=head;
-        ListNode f=head;
-       while(f.next!=null&&f.next.next!=null){
-        s=s.next;
-        f=f.next.next;
-         if(s==f) return true;
-       }
-       
+        ListNode f=head.next;
+
+
+
+        while(f.next!=null&&f.next.next!=null){
+            s=s.next;
+            f=f.next.next;
+            if(s==f) return true;
+        }
         return false;
     }
 }
