44bbd1c8df84c253fb7986a1623c97470aca5914
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-02-26 10:33:19 +0530

    Time: 589 ms (5.04%), Space: 51.2 MB (86.3%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,31 @@
+class MyHashSet {
+    List<Integer> lst;
+    public MyHashSet() {
+        lst=new ArrayList<>();
+        
+    }
+    
+    public void add(int key) {
+        if(!lst.contains(key)){
+            lst.add(key);
+        }
+    }
+    
+    public void remove(int key) {
+        if(lst.contains(key)){
+            lst.remove(Integer.valueOf(key));
+        }
+    }
+    
+    public boolean contains(int key) {
+        return lst.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
