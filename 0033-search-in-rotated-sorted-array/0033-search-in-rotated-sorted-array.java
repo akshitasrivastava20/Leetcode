40705991ae00796e6dc36660912bc250be6dc42a
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-03-26 17:02:14 +0530

    Time: 0 ms (100%), Space: 43.7 MB (88.44%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,25 +1,24 @@
 class Solution {
     public int search(int[] nums, int target) {
-        //find pivot
-        int l=0;
-        int h=nums.length-1;
-        int pivot;
-        while(l<h){
-            int mid=l+(h-l)/2;
-            if(nums[mid]>nums[h])l=mid+1;
-            else h=mid;
-            
+        //find the pivot
+        int s=0;int e=nums.length-1;
+        while(s<e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]>nums[e]) s=mid+1;
+           else  e=mid;
+
         }
-        pivot=l;
-        //find target
-        l=0;
-        h=nums.length-1;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            int realmid=(mid+pivot)%nums.length;
-            if(nums[realmid]==target) return realmid;
-            else if(nums[realmid]>target)h=mid-1;
-            else l=mid+1;
+        int pivot=s;
+        //find tRGET
+       s=0;
+        e=nums.length-1;
+        while(s<=e){
+             int mid=s+(e-s)/2;
+            int real=(mid+pivot)%nums.length;
+            if(nums[real]==target) return real;
+            else if(nums[real]>target) e=mid-1;
+            else s=mid+1;
+
         }
         return -1;
     }
