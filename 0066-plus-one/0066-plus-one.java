ddff192f4d0e73cdec2c5d66ef0aafa2232de34a
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-04-21 23:13:06 +0530

    Time: 0 ms (100%), Space: 43.6 MB (26.96%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,24 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int k=1;
-        int[] newdigits=new int[digits.length+1];
-
-
         for(int i=digits.length-1;i>=0;i--){
-            int sum=digits[i]+k;
-               digits[i]=sum%10;
-               k=sum/10;
-        }
-        if(k==0){
-           return digits;
-        }
-        else{
-            newdigits[0]=1;
-            for(int i=0;i<digits.length;i++){
-                newdigits[i+1]=digits[i];
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
-            return newdigits;
+            digits[i]=0;
         }
-        
+
+        digits=new int[digits.length+1];
+        digits[0]=1;
+        return digits;
     }
 }
