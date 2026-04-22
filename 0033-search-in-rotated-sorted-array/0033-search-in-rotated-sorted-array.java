2526b4ec2c04d1cfe9a955215e28e92cf39058b6
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-04-22 13:50:48 +0530

    Time: 0 ms (100%), Space: 44 MB (30.02%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,24 +1,25 @@
 class Solution {
     public int search(int[] nums, int target) {
-        //find the pivot
-        int s=0;int e=nums.length-1;
+        int s=0;int e=nums.length-1;int pivot=0;
         while(s<e){
             int mid=s+(e-s)/2;
-            if(nums[mid]>nums[e]) s=mid+1;
-           else  e=mid;
-
+            if(nums[mid]<nums[e]){
+                e=mid;
+            }
+            else{
+                s=mid+1;
+            }
         }
-        int pivot=s;
-        //find tRGET
-       s=0;
+        pivot=s;
+        s=0;
         e=nums.length-1;
-        while(s<=e){
-             int mid=s+(e-s)/2;
-            int real=(mid+pivot)%nums.length;
-            if(nums[real]==target) return real;
-            else if(nums[real]>target) e=mid-1;
-            else s=mid+1;
 
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            int realmid=(mid+pivot)%nums.length;
+            if(nums[realmid]==target) return realmid;
+            else if(nums[realmid]<target) s=mid+1;
+            else e=mid-1;
         }
         return -1;
     }
