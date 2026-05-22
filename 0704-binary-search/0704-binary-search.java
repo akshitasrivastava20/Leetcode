69099d1cada98192f55fe55b260672a5933f9738
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-05-22 11:14:09 +0530

    Time: 0 ms (100%), Space: 48.4 MB (49.43%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -4,9 +4,7 @@ class Solution {
         while(s<=e){
             int mid=s+(e-s)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[mid]>target){
-                e=mid-1;
-            }
+            else if(nums[mid]>target) e=mid-1;
             else s=mid+1;
         }
         return -1;
