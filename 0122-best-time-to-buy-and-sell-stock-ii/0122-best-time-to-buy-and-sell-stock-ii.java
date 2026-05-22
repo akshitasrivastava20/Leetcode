8f9264d3472aa3d8dad5d798f052e0f476ee040d
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-05-22 12:20:44 +0530

    Time: 0 ms (100%), Space: 46.3 MB (64.96%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,12 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //consider each day as selling day 
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                profit += prices[i] - prices[i - 1];
-
+        int profit=0;
+        int min=prices[0];
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>min){
+                profit+=prices[i]-min;
             }
+            
+            min=prices[i];
+            
+            
         }
         return profit;
     }
