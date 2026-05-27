603748157b7c07557dbbcfe5b7229ffda22a9f42
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-05-27 14:02:03 +0530

    Time: 2 ms (95.27%), Space: 64 MB (94.66%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,22 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] pro=new int[nums.length];
-        int[] cro=new int[nums.length];
-        int mul=1;int suffix=1;int prefix=1;
+        int[] prefix=new int[nums.length];
+        int[] suffix=new int[nums.length];
+        int p=1;
+        prefix[0]=1;
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]*nums[i-1];
+
+        }
+        suffix[suffix.length-1]=1;
+        for(int i=suffix.length-2;i>=0;i--){
+            suffix[i]=suffix[i+1]*nums[i+1];  
+        }
+         for(int i=0;i<prefix.length;i++){
+            prefix[i]=prefix[i]*suffix[i];
+
+        }
+        return prefix;
         
-        for(int i=0;i<pro.length;i++){
-            pro[i]=prefix;
-            prefix=prefix*nums[i];
-        }
-        for(int i=nums.length-1;i>=0;i--){
-           cro[i]=suffix;
-           suffix=suffix*nums[i];
-
-        }
-        for(int i=0;i<nums.length;i++){
-            nums[i]=pro[i]*cro[i];
-        }
-        return nums;
-
     }
 }
