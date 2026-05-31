25992a4a08cd040f9a09c1c2f64cf9e9037f20e8
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-05-31 16:43:32 +0530

    Time: 4 ms (49.2%), Space: 45.2 MB (40.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb=new StringBuilder();
-
-        for(char ch:s.toLowerCase().toCharArray()){
+        for(char ch:s.toCharArray()){
             if(Character.isLetterOrDigit(ch)){
-               sb.append(ch);
+                sb.append(Character.toLowerCase(ch));
             }
-
 
         }
         String s1=sb.toString();
         String s2=sb.reverse().toString();
-        if(s1.equals(s2)) return true;
-        return false;
+        return s1.equals(s2);
     }
 }
