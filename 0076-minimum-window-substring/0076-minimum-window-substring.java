e33a70ceed1e9c976a130adecca8eb04f9c13466
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-06-01 17:56:46 +0530

    Time: 14 ms (63.58%), Space: 47.4 MB (42.36%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,15 +1,19 @@
 class Solution {
     public String minWindow(String s, String t) {
         if(t.length()>s.length()) return "";
-        int n=s.length();
         HashMap<Character,Integer> mp=new HashMap<>();
-        int i=0;int j=0;int count =0;int len=Integer.MAX_VALUE;int min_start=0;
+        int count=0;
+        int min_start=Integer.MAX_VALUE;
+        int len=Integer.MAX_VALUE;
+        
         for(char c:t.toCharArray()){
             mp.put(c,mp.getOrDefault(c,0)+1);
         }
+        int i=0;int j=0;int n=s.length();
         while(j<n){
-           char ch=s.charAt(j);
-          mp.put(ch,mp.getOrDefault(ch,0)-1);
+            char ch=s.charAt(j);
+
+            mp.put(ch,mp.getOrDefault(ch,0)-1);
             if(mp.get(ch)>=0) count++;
             while(count==t.length()){
                 if(len>j-i+1){
@@ -17,20 +21,15 @@ class Solution {
                     min_start=i;
                 }
                 mp.put(s.charAt(i),mp.get(s.charAt(i))+1);
-
-                
                 if(mp.get(s.charAt(i))>0) count--;
                 i++;
 
-                
             }
+
             j++;
-
         }
-        if(len==Integer.MAX_VALUE){
-            return "";
-        }
-        else return s.substring(min_start,min_start+len);
+        return (len==Integer.MAX_VALUE)?"":s.substring(min_start,min_start+len);
 
+        
     }
 }
