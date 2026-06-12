88b177b85d84fed5d4943a67b3d990ec400f294f
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-06-12 20:26:12 +0530

    Time: 0 ms (100%), Space: 43.1 MB (16.01%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,14 +10,12 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        if(head==null||head.next==null) return head;
-        ListNode f=head;
         ListNode s=head;
+        ListNode f=head;
         while(f!=null&&f.next!=null){
-            f=f.next.next;
             s=s.next;
+            f=f.next.next;
         }
         return s;
-        
     }
 }
