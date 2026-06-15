b66dd227e138ffea03d22eefaa2200d299f578e6
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-06-15 20:53:24 +0530

    Time: 4 ms (74.78%), Space: 45.5 MB (11.14%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,26 +3,27 @@ class Solution {
         int[] ans=new int[nums1.length];
         Stack<Integer> st=new Stack<>();
         HashMap<Integer,Integer> mp=new HashMap<>();
-        //ele,greater
         for(int i=0;i<nums2.length;i++){
-            while(!st.isEmpty()&&(nums2[i]>st.peek())){
-                {
-                    mp.put(st.pop(),nums2[i]);
-
-                }
-
+            while(!st.isEmpty()&&st.peek()<nums2[i]){
+                mp.put(st.pop(),nums2[i]);
             }
             st.push(nums2[i]);
-        }
-        while(!st.isEmpty()){
-            mp.put(st.pop(),-1);
+
 
         }
-        int k=0;
-        for(int n:nums1){
-            ans[k++]=mp.get(n);
+        for(int i=0;i<nums1.length;i++){
+            if(mp.containsKey(nums1[i])){
+                ans[i]=mp.get(nums1[i]);
+            }
+            else{
+                ans[i]=-1;
+            }
+
         }
+
         return ans;
 
+
+        
     }
 }
