ec707fa477886e21d6d0f094fa95a4f10b400f04
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-06-22 15:46:56 +0530

    Time: 1 ms (99.86%), Space: 45.6 MB (32.35%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,25 +1,21 @@
 class Solution {
     public int compress(char[] chars) {
-        int c;
-        int i=0;
-        int idx=0;
+        int i=0;int c;int idx=0;
         while(i<chars.length){
             char curr=chars[i];
             c=0;
             while(i<chars.length&&chars[i]==curr){
-                c++;i++;
+                i++;c++;
+
             }
             chars[idx++]=curr;
             if(c>1){
                 for(char ch:String.valueOf(c).toCharArray()){
                     chars[idx++]=ch;
                 }
-                
             }
-
-
-
         }
         return idx;
+        
     }
 }
