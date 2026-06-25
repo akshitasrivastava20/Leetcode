a4e7ab4b8440105ad0c0d753983cb072bf93765b
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-06-26 00:03:21 +0530

    Time: 8 ms (91.16%), Space: 48.6 MB (95.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
         List<int[]> lst=new ArrayList<>();
+        Arrays.sort(intervals,(a,b)->
+            Integer.compare(a[0],b[0]));
         int s=intervals[0][0];
         int e=intervals[0][1];
         for(int i=1;i<intervals.length;i++){
@@ -17,6 +18,8 @@ class Solution {
         }
         lst.add(new int[]{s,e});
 
-       return lst.toArray(new int[lst.size()][2]);
+         return lst.toArray(new int[lst.size()][2]);
+
+        
     }
 }
