98fead2c6b42c56cd3b466b57b3681b16e62a648
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-07-26 09:00:08 +0530

    Time: 1 ms (99.89%), Space: 55.8 MB (32.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate=0;
-        int count=0;
-        
-        for(int i=0;i<nums.length;i++){
-            if(count==0){
-                candidate=nums[i];
-                count=1;
-            }
-            else if(nums[i]==candidate){
-                count++;
-            }
-            else if (nums[i]!=candidate){
-                count--;
-            }
+        //just like elections
+        int majority=0;
+        int res=0;
+        for(int n:nums){
+            if(majority==0){
+                res=n;
+                majority+=1;
 
+            }
+            else{
+                if(n==res) majority+=1;
+                else majority-=1;
+            }
         }
-        return candidate;
+        return res;
 
+      
+        
     }
 }
