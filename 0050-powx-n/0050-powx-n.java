08ac77ee3586bec0fdbffcb0789c46c06def6269
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-07-26 13:57:51 +0530

    Time: 0 ms (100%), Space: 47.9 MB (65.32%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-
-        if (n < 0) {
-            x = 1 / x;
-            n = -n;
+        double res=1;
+        if(n<0){
+            x=1/x;
+            n=-n;
         }
-        double res = 1;
-        while (n != 0) {
-            if ((n % 2) != 0) {
-                res *= x;
+        while(n!=0){
+            if((n%2)!=0){
+                res*=x;
+
 
             }
-            x *= x;
-            n /= 2;
+            x*=x;
+            n/=2;
         }
         return res;
-
+        
     }
 }
