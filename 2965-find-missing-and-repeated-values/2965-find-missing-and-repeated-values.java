faee0230ca0ef61ba7cedd80e8689b7c7de1bb0e
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-07-26 09:33:12 +0530

    Time: 1 ms (99.8%), Space: 47.5 MB (41.91%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,27 +1,30 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int[] ans=new int[2];
-        int n=grid.length;
-        HashMap<Integer,Integer> mp=new HashMap<>(); 
-        for(int[] row:grid){
-            for(int ele:row){
-                mp.put(ele,mp.getOrDefault(ele,0)+1);
 
-                
+        int[] ans = new int[2];
 
+        int n = grid.length;
+        long N = (long) n * n;
+
+        long expectedSum = N * (N + 1) / 2;
+        long expectedSquareSum = N * (N + 1) * (2 * N + 1) / 6;
+
+        long actualSum = 0;
+        long actualSquareSum = 0;
+
+        for (int[] row : grid) {
+            for (int num : row) {
+                actualSum += num;
+                actualSquareSum += (long) num * num;
             }
         }
-        int sum=0;
-        for(int i:mp.keySet()){
-            if(mp.get(i)==2) ans[0]=i;
-            sum+=i;
 
-        }
-        
-        int total=n*n;
-        ans[1]=total*(total+1)/2-sum;
+        long diff = actualSum - expectedSum;                 // r - m
+        long sum = (actualSquareSum - expectedSquareSum) / diff; // r + m
+
+        ans[0] = (int) ((sum + diff) / 2); // repeated
+        ans[1] = (int) (sum - ans[0]);     // missing
+
         return ans;
-
-
     }
 }
