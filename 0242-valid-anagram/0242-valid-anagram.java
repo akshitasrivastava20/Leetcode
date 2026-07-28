a530783ccf8ed9699ed0c40d02fbf84f46d17282
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-07-28 20:32:19 +0530

    Time: 29 ms (5.41%), Space: 46.8 MB (9.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        HashMap<Character,Integer> mp=new HashMap<>();
         HashMap<Character,Integer> mp1=new HashMap<>();
+        HashMap<Character,Integer> mp2=new HashMap<>();
+
         for(int i=0;i<s.length();i++){
-            mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
-            mp1.put(t.charAt(i),mp1.getOrDefault(t.charAt(i),0)+1);
-
+            mp1.put(s.charAt(i),mp1.getOrDefault(s.charAt(i),0)+1);
+            mp2.put(t.charAt(i),mp2.getOrDefault(t.charAt(i),0)+1);
         }
-        if(mp.equals(mp1)) return true;
-        return false;
+
+        return (mp1.equals(mp2));
 
 
+        
     }
 }
