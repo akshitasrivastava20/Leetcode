0e8f4545a9fda0bdf3e7e4feb272992468122ba0
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-07-28 20:20:17 +0530

    Time: 5 ms (82.8%), Space: 77.5 MB (29.47%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -5,11 +5,15 @@ class Solution {
         int area=0;
         while(i<j){
             area=Math.max(area,Math.min(height[i],height[j])*(j-i));
-            if(height[i]<height[j]) i++;
-            else j--;
-
+            if(height[i]<height[j]){
+                i++;
+            }
+            else {
+                j--;
+            }
 
         }
         return area;
+        
     }
 }
