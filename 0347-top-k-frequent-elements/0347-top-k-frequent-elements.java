0003989df44dda88792bd2bb492c3316d5ebcb1e
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-08-10 12:16:13 +0530

    Time: 16 ms (37.95%), Space: 47.6 MB (67.35%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,43 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        
-        int[] ans = new int[k];
-        int p = 0;
-        int n = nums.length;
-
-        HashMap<Integer, Integer> mp = new HashMap<>();
-
-        // Step 1: Frequency map
-        for (int num : nums) {
-            mp.put(num, mp.getOrDefault(num, 0) + 1);
+        int[] ans=new int[k];
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        for(int i:nums){
+            mp.put(i,mp.getOrDefault(i,0)+1);
         }
+        ArrayList<Integer> lst=new ArrayList<>(mp.keySet());
 
-        // Step 2: Bucket array
-        List<Integer>[] bucket = new List[n + 1];
+        lst.sort((a,b)->mp.get(b)-mp.get(a));
 
-        for (int num : mp.keySet()) {
-            int freq = mp.get(num);
-
-            if (bucket[freq] == null) {
-                bucket[freq] = new ArrayList<>();
-            }
-
-            bucket[freq].add(num);
+        for(int i=0;i<k;i++){
+            ans[i]=lst.get(i);
         }
-
-        // Step 3: Traverse from high freq
-        for (int i = n; i >= 0 && p < k; i--) {
-
-            if (bucket[i] != null) {
-
-                for (int num : bucket[i]) {
-                    ans[p++] = num;
-
-                    if (p == k) break;
-                }
-            }
-        }
-
         return ans;
+
+
+
+        
     }
 }
