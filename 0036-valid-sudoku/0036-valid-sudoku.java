d58d299da2c3ed9d5370ba2ab994ede43334d7a7
AKSHITA SRIVASTAVA <akshita.srivastava.1505@gmail.com>
2026-08-11 11:25:27 +0530

    Time: 2 ms (67.1%), Space: 46.3 MB (84.36%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,46 +1,46 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-       HashSet<Character> s=new HashSet<>();
-       HashSet<Character> p=new HashSet<>();
-
-       
-       for(int i=0;i<9;i++){
-        int c1=0;int c2=0;
-        for(int j=0;j<9;j++){
-            if(board[i][j]!='.') 
-            {
-                s.add(board[i][j]);
-                c1++;
-
-             }
-            if(board[j][i]!='.'){
-                p.add(board[j][i]);
-                c2++;
-
-            } 
-             
-        }
-        if(s.size()!=c1||p.size()!=c2) return false;
-        s.clear();
-        p.clear();
-
-       }
-       for(int row=0;row<9;row+=3){
-        for(int col=0;col<9;col+=3){
-            int c1=0;
-            for(int i=row;i<row+3;i++){
-                for(int j=col;j<col+3;j++){
-                    if(board[i][j]!='.'){
-                    s.add(board[i][j]);
-                    c1++;}
-                }
+        HashSet<Character> set=new HashSet<>();
+        for(int i=0;i<9;i++){
+            
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                if(!set.add(board[i][j])) return false;
             }
-            if(s.size()!=c1) return false;
-            s.clear();
+            set.clear();
 
         }
-       }
-       return true;
-       
+
+        for(int j=0;j<9;j++){
+             
+            for(int i=0;i<9;i++){
+                if(board[i][j]=='.') continue;
+                 if(!set.add(board[i][j])) return false;
+
+            }
+            set.clear();
+        }
+
+        for(int i=0;i<9;i+=3){
+            for(int j=0;j<9;j+=3){
+                int r=i+3;
+                int s=j+3;
+                for(int p=i;p<r;p++){
+                    for(int q=j;q<s;q++){
+                        if(board[p][q]=='.')continue;
+                         if(!set.add(board[p][q])) return false;
+
+                    }
+                }
+
+                set.clear();
+
+
+
+            }
+        }
+
+        return true;
+        
     }
 }
